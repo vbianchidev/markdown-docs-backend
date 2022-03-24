7bf96ab617501e4b0c37e649b969c0c017fc4d22
Vitor Bianchi <41026988+VitorBianchi@users.noreply.github.com>
2022-03-23 22:02:35 -0300

    delete core folder

--- src/modules/page/page.service.ts
+++ src/modules/page/page.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { MongoGenericRepository } from 'src/core/base/mongo.generic.repository';
+import { MongoGenericRepository } from 'src/base/mongo.generic.repository';
 
 import { CreatePageDto } from './dto/create-page.dto';
 import { Page } from './entities/page.entity';
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { MongoGenericRepository } from 'src/core/base/mongo.generic.repository';
+import { MongoGenericRepository } from 'src/base/mongo.generic.repository';
 
 import { CreateUserDto } from './dto/create-user-dto';
 import { User } from './entities/user.entity';
